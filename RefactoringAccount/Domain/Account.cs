7c23fbe835d281b5404f9bbaa085fb7572bc4ac3
Matias <mnfredes@gmail.com>
2021-01-14 11:25:23 -0300

    replace magic number by constants

--- RefactoringAccount/Domain/Account.cs
+++ RefactoringAccount/Domain/Account.cs
@@ -16,6 +16,11 @@ namespace Domain
             private set;
         }
 
+        public Account(AccountType type)
+        {
+            this.type = type;
+        }
+
         public virtual void AddTransaction(decimal amount)
         {
             RewardPoints += CalculateRewardPoints(amount);
@@ -28,13 +33,15 @@ namespace Domain
             switch (type)
             {
                 case AccountType.Silver:
-                    points = (int)decimal.Floor(amount / 10);
+                    points = (int)decimal.Floor(amount / SilverTransactionCostPerPoint);
                     break;
                 case AccountType.Gold:
-                    points = (int)decimal.Floor((Balance / 10000 * 5) + (amount / 5));
+                    points = (int)decimal.Floor((Balance / GoldBalanceCostPerPoint * GoldTransactionCostPerPoint) + 
+                        (amount / GoldTransactionCostPerPoint));
                     break;
                 case AccountType.Platinum:
-                    points = (int)decimal.Ceiling((Balance / 10000 * 10) + (amount / 2));
+                    points = (int)decimal.Ceiling((Balance / PlatinumBalanceCostPerPoint * PlatinumTransactionCostPerPoint) + 
+                        (amount / PlatinumTransactionCostPerPoint));
                     break;
                 default:
                     points = 0;
@@ -46,9 +53,13 @@ namespace Domain
 
         private readonly AccountType type;
 
-        public Account(AccountType type)
-        {
-            this.type = type;
-        }
+        private const int SilverTransactionCostPerPoint = 10;
+        private const int GoldTransactionCostPerPoint = 5;
+        private const int PlatinumTransactionCostPerPoint = 2;
+
+        private const int GoldBalanceCostPerPoint = 2000;
+        private const int PlatinumBalanceCostPerPoint = 1000;
+
+       
     }
 }
