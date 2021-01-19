2ea6637c41fae11f5e2f4d7634714e1c6b952958
Matias <mnfredes@gmail.com>
2021-01-19 14:20:41 -0300

    SRP trade processor example

---
+++ RefactoringAccount/Domain/IRewardCard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public interface IRewardCard
+    {
+        public int RewardPoints
+        {
+            get;
+        }
+
+        int CalculateRewardPoints(decimal amount);
+
+    }
+}
